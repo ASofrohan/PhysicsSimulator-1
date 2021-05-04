699255c86c3dd28a6e04d8625deb328eff579a50
Germief <60486644+Germief@users.noreply.github.com>
2021-05-05 00:22:49 +0200

    Update ChangeForceClassDialog.java

--- src/simulator/view/ChangeForceClassDialog.java
+++ src/simulator/view/ChangeForceClassDialog.java
@@ -1,13 +1,23 @@
 package simulator.view;
 
+import java.awt.BorderLayout;
 import java.awt.Dialog;
+import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.GraphicsConfiguration;
 import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
 
 import org.json.JSONObject;
 
@@ -38,7 +48,50 @@ public class ChangeForceClassDialog extends JDialog  implements SimulatorObserve
 	}
 	
 	private void initGUI(){
+		this.setLayout(new BorderLayout(15, 20));
+		JLabel descripcion   = new JLabel("Select a force law and provide values for the parameters "
+			+ "in the Value column (default values are used for parameters with no value) ");
+		this.add(descripcion, BorderLayout.NORTH);
 		
+		JPanel centro = new JPanel();
+		centro.setLayout(new BoxLayout(centro, BoxLayout.X_AXIS));
+		
+		
+		
+		this.add(centro, BorderLayout.CENTER);
+				
+		JPanel botones = new JPanel();
+		JButton cancel = new JButton("Cancel");
+		cancel.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+			}						
+		});
+		
+		JButton ok = new JButton("Ok");
+		ok.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+
+			}			
+		});
+		
+		JLabel Fuerzas   = new JLabel("Forcelaw:");
+		centro.add(Fuerzas);
+		idsc = new JComboBox<String>();
+		idsc.setMaximumSize(new Dimension(500,20));
+		botones.add(idsc);
+		
+		botones.add(cancel);
+		botones.add(ok);
+		this.add(botones, BorderLayout.SOUTH);
+		
+		
+		this.setPreferredSize(new Dimension(700, 200));
+		this.pack();
+
 	}
 
 
